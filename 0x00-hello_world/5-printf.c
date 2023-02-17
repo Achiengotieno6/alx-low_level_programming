f92e56a98e60db0a6c1287cf732107937740a528
Achiengotieno6 <audrey.achieng@gmail.com>
2023-02-16 18:51:38 -0800

    pritf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: This funtion prints a message using printf()
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+
+}
