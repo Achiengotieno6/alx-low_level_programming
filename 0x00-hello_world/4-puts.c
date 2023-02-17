dfb35fc129102607303cce5918399451a0b84c27
Achiengotieno6 <audrey.achieng@gmail.com>
2023-02-16 18:38:04 -0800

    hello puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: This function will print the message using the puts() funtion
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+
+}
+
+
