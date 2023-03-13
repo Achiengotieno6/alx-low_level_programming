6be43a6e339f5c30e3aa387715813001c7fe20cb
Achiengotieno6 <audrey.achieng@gmail.com>
2023-03-13 05:26:27 -0700

    number of coins

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - entry point
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Return: 0 on success, 1 on error
+ */
+
+int main(int argc, char **argv)
+{
+	int cents = 0;
+	int coins = 0;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	cents = atoi(argv[1]);
+
+	if (cents < 0)
+	{
+		printf("0\n");
+		return (0);
+	}
+	while (cents >= 25)
+	{
+		coins++;
+		cents -= 25;
+	}
+	while (cents >= 10)
+	{
+		coins++;
+		cents -= 10;
+	}
+	while (cents >= 5)
+	{
+		cents++;
+		coins -= 5;
+	}
+	while (cents >= 2)
+	{
+		coins++;
+		cents -= 2;
+	}
+	while (cents >= 1)
+	{
+		coins++;
+		cents -=1;
+	}
+	printf("%d\n", coins);
+	return (0);
+}
