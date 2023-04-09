4d853841629863b1506bb10d854f835e9e917562
Achiengotieno6 <audrey.achieng@gmail.com>
2023-04-09 04:27:11 -0700

    compiled 100-get_endianness.c

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * get_endianness - gets the order of byte storage
+ * Return: 1 or 0
+ *
+ */
+int get_endianness(void)
+{
+	unsigned int a = 1;
+	char *c;
+
+	c = (char *) &a;
+	if (*c)
+		return (1);
+	else
+		return (0);
+}
