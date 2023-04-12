4617bc08eb276c5cb70701d7d0f4eb289a87514b
Achiengotieno6 <audrey.achieng@gmail.com>
2023-04-12 04:42:17 -0700

    recompiled 0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -7,28 +7,32 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fp;
-	ssize_t n, m;
+	int Audrey_file;
+	ssize_t letters_w, letters_r;
 	char *buf;
 
 	if (filename == NULL)
 		return (0);
+	/* open the file */
+	Audrey_file = open(filename, O_RDONLY);
 
-	fp = open(filename, O_RDONLY);
-
-	if (fp == -1)
+	if (Audrey_file == -1)
 		return (0);
-
+	/* allocate memory */
 	buf = malloc(sizeof(char) * (letters));
 	if (buf == NULL)
 		return (0);
+	/* read file  and store data in buf */
+	letters_r = read(Audrey_file, buf, letters);
 
-	n = read(fp, buf, letters);
-	m = write(STDOUT_FILENO, buf, n);
+	if (letters_r == -1)
+		return (0);
 
-	close(fp);
+	letters_w = write(STDOUT_FILENO, buf, letters_r);
+	if (letters_w != letters_r)
+		return (0);
 
 	free(buf);
-
-	return (m);
+	close(Audrey_file);
+	return (letters_r);
 }
