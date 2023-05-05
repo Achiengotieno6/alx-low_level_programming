b0f308ca649d074c99aee0dc0e717abef733a9df
Achiengotieno6 <audrey.achieng@gmail.com>
2023-05-05 01:34:00 +0000

    compiled get bit code

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * get_bit - gives value of bit at a given index
+ * @n: the range of numbe
+ * @index: the index considered
+ * Return: value of the bit at index, -1
+ *
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	if (index >= sizeof(unsigned long int) * 8)
+		return (-1);
+	return ((n >> index) & 1);
+}
