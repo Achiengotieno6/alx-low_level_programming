cb00a1ae7d297f06042f51641390aa50f9e34ff1
Achiengotieno6 <audrey.achieng@gmail.com>
2023-05-05 01:13:53 +0000

    recompilecode

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -8,8 +8,8 @@ void print_binary(unsigned long int n)
 {
 	if (n > 1)
 		print_binary(n >> 1);
-	if ((n & 1) == 1)
-		_putchar('1');
 	if ((n & 1) == 0)
 		_putchar('0');
+	if ((n & 1) == 1)
+		_putchar('1');
 }
