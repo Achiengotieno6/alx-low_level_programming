dac25bd26c501e08408924ea7c675df00805e57b
Achiengotieno6 <audrey.achieng@gmail.com>
2023-05-05 00:57:02 +0000

    compiled 0-binary_to_uint.c

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * binary_to_uint - converts binary number to decimal number
+ * @b: string of binary numbers
+ * Return: result of conversion or 0
+ *
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int decimal = 0;
+	int len = 0;
+
+	if (b == NULL)
+		return (0);
+	while (b[len] != '0')
+	{
+		if (b[len] != '0' && b[len] != '1')
+			return (0);
+		len++;
+	}
+	while (*b != '\0')
+	{
+		decimal = (decimal << 1) + (*b++ - '0');
+	}
+	return (decimal);
+}
